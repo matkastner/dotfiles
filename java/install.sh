e2727ff3e60f6cf03b65591b557c2b4e673e0e76
Mat Kastner <mat@dabapps.com>
2021-09-09 18:14:14 +0100

    add java install

--- java/install.sh
+++ java/install.sh
@@ -3,3 +3,14 @@ then
     echo "› brew install adoptopenjdk/openjdk/adoptopenjdk8"
     brew install adoptopenjdk/openjdk/adoptopenjdk8
 fi
+
+if ! brew list | grep jenv > /dev/null
+then
+    echo "› brew install jenv"
+    brew install jenv
+fi
+
+for jdk in /Library/Java/JavaVirtualMachines/*.jdk
+do
+    jenv add "$jdk/Contents/Home/"
+done

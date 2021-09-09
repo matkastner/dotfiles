e9d3e05de69a6d6862f7b8bd492c4607cf8ecef9
Mat Kastner <mat@dabapps.com>
2021-09-09 17:35:57 +0100

    only use sdkmanager if it exists

--- android-studio/install.sh
+++ android-studio/install.sh
@@ -1,4 +1,7 @@
 brew install android-studio
 
-/Users/mat/Library/Android/sdk/tools/bin/sdkmanager "platforms;android-29" "system-images;android-29;default;x86_64" "system-images;android-29;google_apis;x86"
-/Users/mat/Library/Android/sdk/tools/bin/sdkmanager "cmdline-tools;latest" "build-tools;29.0.2"
+if [ -a ~/Library/Android/sdk/tools/bin/sdkmanager ]
+then
+    ~/Library/Android/sdk/tools/bin/sdkmanager "platforms;android-29" "system-images;android-29;default;x86_64" "system-images;android-29;google_apis;x86"
+    ~/Library/Android/sdk/tools/bin/sdkmanager "cmdline-tools;latest" "build-tools;29.0.2"
+fi

c003efffb288af3dabbad3141ac6aeb96d66596e
Mat Kastner <mat@dabapps.com>
2021-09-10 12:24:17 +0100

    improve repo retrieve

--- github/functions.zsh
+++ github/functions.zsh
@@ -22,14 +22,16 @@ get_repos() {
     mkdir -p ~/Code/dabapps
     cd ~/Code/dabapps
 
-    for repo in $(gh repo list dabapps --json nameWithOwner | jq -r '.[].name' | grep -i "${1}")
+    search="$(echo "${1}" | tr '[:upper:]' '[:lower:]')"
+
+    for repo in $(gh repo list dabapps -L 500 --json name -q ".[].name | select(contains(\"${search}\"))")
     do
         if [ -d ${repo} ]
         then
             echo "Skipping ${repo}..."
         else
             echo "Cloning ${repo}..."
-            gh repo clone ${repo}
+            gh repo clone dabapps/${repo}
         fi
     done
 

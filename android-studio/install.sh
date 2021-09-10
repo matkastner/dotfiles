64b04769efc10a76a945f3a245f68fd262f640be
Mat Kastner <mat@dabapps.com>
2021-09-10 12:50:43 +0100

    add emulator creation

--- android-studio/install.sh
+++ android-studio/install.sh
@@ -12,7 +12,17 @@ then
     fi
 
     yes | sdkmanager --licenses
+    sdkmanager --update
     sdkmanager "platforms;android-29" "system-images;android-29;default;x86_64" "system-images;android-29;google_apis;x86"
     sdkmanager "cmdline-tools;latest" "build-tools;29.0.2"
-    sdkmanager --update
+fi
+
+if test $(which avdmanager)
+then
+    if test $(which jenv)
+    then
+        jenv local 1.8
+    fi
+
+    avdmanager create avd -n dabapps -d "pixel_xl" -k "system-images;android-29;google_apis;x86" --force
 fi

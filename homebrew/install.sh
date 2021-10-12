6d546168784adfb8778a29840b2d09049d4d682a
Mat Kastner <mat@dabapps.com>
2021-10-12 11:31:14 +0100

    fix homebrew of /opt/

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -11,6 +11,11 @@ then
   echo "  Installing Homebrew for you."
 
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  if [ -d /opt/homebrew/bin/ ]
+  then
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
 else
   brew update
   brew upgrade
---
+++ homebrew/path.zsh
@@ -0,0 +1,4 @@
+if [ -d /opt/homebrew/bin/ ]
+then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi

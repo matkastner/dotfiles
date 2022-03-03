b6a96a1f444b78dcc542fe4ec3f546297f808b4e
Mat Kastner <mat@dabapps.com>
2022-03-03 10:25:09 +0000

    add better pyenv instal

--- macos/functions.zsh
+++ macos/functions.zsh
@@ -1,7 +1,7 @@
-function cleanup_dependencies {
-    find ~/Code -type d -name node_modules -prune -exec rm -rf '{}' +
-    find ~/Code -type d -name Pods -prune -exec rm -rf '{}' +
-    find ~/Code -type f -name '*.hprof' -prune -exec rm '{}' +
+function cleanup {
+    find ~/Code -type d -name node_modules -prune -exec trash '{}' +
+    find ~/Code -type d -name Pods -prune -exec trash '{}' +
+    find ~/Code -type f -name '*.hprof' -prune -exec trash '{}' +
 
-    find ~/Downloads -mtime +14 -exec rm -rf '{}' +
+    find ~/Downloads -mtime +14 -maxdepth 1 -exec trash '{}' +
 }
--- python/functions.zsh
+++ python/functions.zsh
@@ -1,5 +1,42 @@
 function pyenv_install {
-  CFLAGS="-I$(brew --prefix openssl)/include -I$(brew --prefix bzip2)/include -I$(brew --prefix readline)/include -I$(xcrun --show-sdk-path)/usr/include" LDFLAGS="-L$(brew --prefix openssl)/lib -L$(brew --prefix readline)/lib -L$(brew --prefix zlib)/lib -L$(brew --prefix bzip2)/lib" arch -x86_64 pyenv install --patch < <(curl -sSL https://github.com/python/cpython/commit/8ea6353.patch\?full_index\=1)
+  export CFLAGS="-I$(brew --prefix openssl)/include -I$(brew --prefix bzip2)/include -I$(brew --prefix readline)/include -I$(xcrun --show-sdk-path)/usr/include" LDFLAGS="-L$(brew --prefix openssl)/lib -L$(brew --prefix readline)/lib -L$(brew --prefix zlib)/lib -L$(brew --prefix bzip2)/lib"
+
+  pyenv install
+
+  if [ ! $? -eq 0 ]
+  then
+    echo
+    echo
+    echo "Installing python failed, trying x86_64 instead..."
+    echo
+    arch -x86_64 pyenv install
+  fi
+
+  if [ ! $? -eq 0 ]
+  then
+    echo
+    echo
+    echo "Installing python failed, trying with patch instead..."
+    echo
+    pyenv install --patch < <(curl -sSL https://github.com/python/cpython/commit/8ea6353.patch\?full_index\=1)
+  fi
+
+  if [ ! $? -eq 0 ]
+  then
+    echo
+    echo
+    echo "Installing python failed, trying with patch and x86_64 instead..."
+    echo
+    arch -x86_64 pyenv install --patch < <(curl -sSL https://github.com/python/cpython/commit/8ea6353.patch\?full_index\=1)
+  fi
+
+  if [ ! $? -eq 0 ]
+  then
+    echo
+    echo
+    echo "Unable to install a working version of Python!"
+    echo
+  fi
 }
 
 function set_python_version {
